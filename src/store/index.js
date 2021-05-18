5a81c750809bfc600f55bf0c47e10041a4b26763
Ilia Klimov <therapyspb@yandex.ru>
2021-05-18 21:29:24 +0300

    добавил логи в logux при разработке

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { CrossTabClient } from '@logux/client';
+import { CrossTabClient, badge, badgeRu, log } from '@logux/client';
+import { badgeStyles } from '@logux/client/badge/styles';
 import { createStoreCreator } from '@logux/redux';
 
 import { reducer } from './reducer';
@@ -11,8 +12,18 @@ const client = new CrossTabClient({
 });
 
 const createStore = createStoreCreator(client);
-
 const store = createStore(reducer);
+
+if (process.env.NODE_ENV === 'development') {
+  log(store.client);
+  badge(store.client, {
+    messages: badgeRu,
+    styles: {
+      ...badgeStyles,
+    },
+    position: 'top-center',
+  });
+}
 
 store.client.start();
 
