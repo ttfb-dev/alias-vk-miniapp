63454b133bcc8f3ea5c380c86e97897bd08b8b51
Ilia Klimov <therapyspb@yandex.ru>
2021-05-26 18:00:40 +0300

    добавил сервис для работы с апи вк

---
+++ src/services/index.js
@@ -0,0 +1,23 @@
+import vkapi from '../api';
+
+const app = {
+  init: () => {
+    vkapi.init();
+  },
+
+  getFriends: async () => {
+    const access_token = await vkapi.getAuthToken({ app_id: 7856384, scope: 'friends' });
+    const friendAppUsers = await vkapi.callAPIMethod({
+      method: 'friends.getAppUsers',
+      params: { v: '5.131', access_token },
+    });
+    const friends = await vkapi.callAPIMethod({
+      method: 'users.get',
+      params: { user_ids: friendAppUsers.join(','), fields: 'photo_50', v: '5.131', access_token },
+    });
+
+    return friends;
+  },
+};
+
+export { app };
