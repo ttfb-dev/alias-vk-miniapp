5f905e7cdcf6eec4da65f7401e8fe0795d57c17e
Ilia Klimov <therapyspb@yandex.ru>
2021-05-27 23:11:27 +0300

    фикс доступа к апи

--- src/api/index.js
+++ src/api/index.js
@@ -12,8 +12,6 @@ class API {
   getAuthToken = async ({ app_id, scope }) => {
     const data = await this.bridge.send('VKWebAppGetAuthToken', { app_id, scope });
 
-    console.warn(data);
-
     return { access_token: data.access_token, scope: data.scope.split(',') };
   };
 
--- src/services/index.js
+++ src/services/index.js
@@ -1,30 +1,27 @@
 import vkapi from '../api';
 
 class App {
-  constructor() {
-    this.access_token = null;
-  }
-
   init = () => {
     vkapi.init();
-    vkapi.getAuthToken({ app_id: 7856384, scope: 'friends' }).then(({ access_token }) => {
-      this.access_token = access_token;
-    });
   };
 
   getUsers = async (userIds) => {
+    const { access_token } = await vkapi.getAuthToken({ app_id: 7856384, scope: 'friends' });
+
     const users = await vkapi.callAPIMethod({
       method: 'users.get',
-      params: { user_ids: userIds.join(','), fields: 'photo_50', v: '5.131', access_token: this.access_token },
+      params: { user_ids: userIds.join(','), fields: 'photo_50', v: '5.131', access_token },
     });
 
     return users;
   };
 
   getFriends = async () => {
+    const { access_token } = await vkapi.getAuthToken({ app_id: 7856384, scope: 'friends' });
+
     const friendAppUsers = await vkapi.callAPIMethod({
       method: 'friends.getAppUsers',
-      params: { v: '5.131', access_token: this.access_token },
+      params: { v: '5.131', access_token },
     });
     const friends = await this.getUsers(friendAppUsers);
 
