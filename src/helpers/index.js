c7f09ed0e5fc35b0082be18cb8f2dc9750e54242
Ilia Klimov <therapyspb@yandex.ru>
2021-05-31 23:25:24 +0300

    добавил хелпер для окончаний при склонениях

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -22,4 +22,10 @@ const queryStringParse = (query) => {
   }, {});
 };
 
-export { queryStringParse };
+const declension = (value, forms) => {
+  const cases = [2, 0, 1, 1, 1, 2];
+
+  return forms[value % 100 > 4 && value % 100 < 20 ? 2 : cases[value % 10 < 5 ? value % 10 : 5]];
+};
+
+export { queryStringParse, declension };
