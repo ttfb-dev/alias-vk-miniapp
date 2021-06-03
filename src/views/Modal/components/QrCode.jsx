84f2f8ddc753866723fe9cc81db2ff2373458839
Ilia Klimov <therapyspb@yandex.ru>
2021-06-03 23:47:59 +0300

    добавил возможность входить по номеру комнаты

--- src/views/Modal/components/QrCode.jsx
+++ src/views/Modal/components/QrCode.jsx
@@ -12,24 +12,21 @@ import styles from './index.module.scss';
 const QrCode = ({ onClose, ...props }) => {
   const dispatch = useDispatch();
 
+  const onScan = async () => {
+    const code = await vkapi.openCodeReader();
+    const url = new URL(code);
+    const hashParams = queryStringParse(url.hash);
+    const roomId = parseInt(hashParams?.roomId, 10);
+
+    if (roomId) {
+      dispatch.sync(room.action.join({ roomId }));
+    }
+  };
+
   return (
     <ModalCard {...props} onClose={onClose} header='Отсканируйте QR-код' subheader='или введите код комнаты'>
       <Div className={styles.actions}>
-        <Button
-          size='l'
-          mode='primary'
-          stretched
-          onClick={async () => {
-            const code = await vkapi.openCodeReader();
-            const url = new URL(code);
-            const hashParams = queryStringParse(url.hash);
-            const roomId = parseInt(hashParams?.roomId, 10);
-
-            if (roomId) {
-              dispatch.sync(room.action.join({ roomId }));
-            }
-          }}
-        >
+        <Button size='l' mode='primary' stretched onClick={onScan}>
           Сканировать
         </Button>
         <Button
