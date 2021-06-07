cc05745fb50bcd120b39b53159a6cebe998809f5
Ilia Klimov <therapyspb@yandex.ru>
2021-06-07 18:55:16 +0300

    добавил редирект на панель хода в вью игры

--- src/views/Game/index.jsx
+++ src/views/Game/index.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useClient } from '@logux/client/react';
 import { useSubscription } from '@logux/redux';
 import { View } from '@vkontakte/vkui';
+
+import { general } from '../../store';
 
 import { Lobby } from './Lobby';
 import { Step } from './Step';
 
 const Game = (props) => {
+  const client = useClient();
+  const dispatch = useDispatch();
   const activePanel = useSelector((state) => state.general.game.activePanel);
   const roomId = useSelector((state) => state.room.roomId);
   const isSubscribing = useSubscription([`room/${roomId}/game`]);
+
+  useEffect(() => {
+    const gameState = client.type(
+      'game/state',
+      (action) => {
+        if (action.game.status === 'step') {
+          dispatch(general.action.route({ activeView: 'game', game: { activePanel: 'step' } }));
+        }
+      },
+      { event: 'add' },
+    );
+
+    return () => {
+      gameState();
+    };
+  }, [client, dispatch]);
 
   return (
     <View {...props} activePanel={activePanel}>
