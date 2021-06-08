6a0eb174d86d39da2fedd11713537011ba93ddc4
Ilia Klimov <therapyspb@yandex.ru>
2021-06-08 19:14:39 +0300

    добавил хук для таймера

---
+++ src/views/Game/hooks/useTimer.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+
+const useTimer = (initialTime, interval = 1000) => {
+  const [status, setStatus] = useState('INITIAL');
+  const [time, setTime] = useState();
+
+  useEffect(() => {
+    setStatus('RUNNING');
+    setTime(initialTime);
+  }, [initialTime]);
+
+  useEffect(() => {
+    if (status !== 'STOPPED' && time === 0) {
+      setStatus('STOPPED');
+      setTime(0);
+    }
+  }, [time, status, initialTime]);
+
+  useEffect(() => {
+    let intervalId = null;
+
+    if (status === 'RUNNING') {
+      intervalId = setInterval(() => {
+        setTime(time - 1);
+      }, interval);
+    } else if (intervalId) {
+      clearInterval(intervalId);
+    }
+
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [status, interval, time]);
+
+  return { time, status };
+};
+
+export { useTimer };
