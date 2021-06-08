f9c89b22aaaa67113da856b014bad74c46285d0e
Orange <bobrov.d.p@readyforsky.com>
2021-06-09 00:06:15 +0300

    rename actions

--- src/store/game/action.js
+++ src/store/game/action.js
@@ -1,9 +1,9 @@
 import { defineAction } from '@logux/actions';
 
-const setStep = defineAction('game/setStep');
+const setStep = defineAction('game/set_step');
 const setStepHistory = defineAction('game/setStepHistory');
 const setWords = defineAction('game/setWords');
 const setStatus = defineAction('game/setStatus');
-const setTimestamp = defineAction('game/setTimestamp');
+const setTimestamp = defineAction('game/set_timestamp');
 
 export { setStep, setStepHistory, setWords, setStatus, setTimestamp };
