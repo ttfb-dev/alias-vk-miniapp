b4891beba04479ed9681c0660b457741954203ea
Ilia Klimov <therapyspb@yandex.ru>
2021-07-01 00:11:38 +0300

    фиксы скопом

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { Page, Router } from '@happysanta/router';
 
 export const PAGE_HOME = '/';
+export const PAGE_ONBOARDING = '/onboarding';
 export const PAGE_ROOM = '/room';
 export const PAGE_LOBBY = '/lobby';
 export const PAGE_STEP = '/step';
@@ -9,6 +10,7 @@ export const VIEW_MAIN = 'view_main';
 export const VIEW_GAME = 'view_game';
 
 export const PANEL_HOME = 'panel_home';
+export const PANEL_ONBOARDING = 'panel_onboarding';
 export const PANEL_ROOM = 'panel_room';
 export const PANEL_LOBBY = 'panel_lobby';
 export const PANEL_STEP = 'panel_step';
@@ -30,6 +32,8 @@ export const POPOUT_GAME_LEAVE = 'popout_game_leave';
 
 const routes = {
   [PAGE_HOME]: new Page(PANEL_HOME, VIEW_MAIN),
+  [PAGE_ONBOARDING]: new Page(PANEL_ONBOARDING, VIEW_MAIN),
+
   [PAGE_ROOM]: new Page(PANEL_ROOM, VIEW_GAME),
   [PAGE_LOBBY]: new Page(PANEL_LOBBY, VIEW_GAME),
   [PAGE_STEP]: new Page(PANEL_STEP, VIEW_GAME),
