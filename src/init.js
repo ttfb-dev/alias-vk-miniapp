3c0dcef064c9ce6c751d26f035f6a5bfeebdb53a
Ilia Klimov <therapyspb@yandex.ru>
2021-07-03 22:32:12 +0300

    обновил инит

--- src/init.js
+++ src/init.js
@@ -8,38 +8,40 @@ import { general, profile, room, store } from '@/store';
 
 import { creds, env, misc } from './config';
 
-let haveToJoinRoom = false;
-
-// store init
-store.client.start();
-if (creds.userId) {
-  store.dispatch(general.action.setUserId({ userId: parseInt(creds.userId, 10) }));
-  store.dispatch.sync(profile.action.getSets());
-}
-if (creds.userId && !misc.roomId) {
-  store.dispatch.sync(room.action.whereIAm());
-} else if (creds.userId && misc.roomId) {
-  haveToJoinRoom = true;
-}
-
 (async () => {
   // app init
   await AppService.initApp();
   const isFinished = await AppService.isOnboardingFinished();
 
+  // store init
+  store.client.start();
+
   // router init
   router.start();
-  if (!isFinished) {
-    router.replacePage(PAGE_ONBOARDING);
-  } else if (haveToJoinRoom) {
-    store.dispatch(room.action.setRoomId({ roomId: misc.roomId }));
-    store.dispatch.sync(room.action.join({ roomId: misc.roomId }));
+
+  if (creds.userId) {
+    // если мы знаем айди, то сохраням его и запрашиваем сеты пользователя
+    store.dispatch(general.action.setUserId({ userId: parseInt(creds.userId, 10) }));
+    store.dispatch.sync(profile.action.getSets());
   }
 
-  // if we have access for friends list then fetch it
-  if (misc.tokenSettings?.includes('friends')) {
-    const friends = await AppService.getFriendProfiles();
+  if (!isFinished) {
+    // если онбординг не пройден, то редиректим туда
+    router.replacePage(PAGE_ONBOARDING);
+  } else {
+    if (creds.userId && !misc.roomId) {
+      // иначе если мы не знаем номер комнаты, то запрашиваем его
+      store.dispatch.sync(room.action.whereIAm());
+    } else if (creds.userId && misc.roomId) {
+      // если номер комнаты известен, то сохраняем номер и делаем джоин к комнате
+      store.dispatch(room.action.setRoomId({ roomId: misc.roomId }));
+      store.dispatch.sync(room.action.join({ roomId: misc.roomId }));
+    }
+  }
 
+  if (misc.tokenSettings?.includes('friends')) {
+    // если у есть права на получение инфы о друзьях пользователя, то запрашиваем этот список
+    const friends = await AppService.getFriendProfiles();
     store.dispatch(general.action.setFriends({ friends }));
   }
 })();
