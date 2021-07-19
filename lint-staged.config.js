192fc7a78e01bee942ddba46a6c497a55dbf8a8a
Ilia Klimov <therapyspb@yandex.ru>
2021-07-19 21:43:31 +0300

    пофиксил команды в lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  '{*.js,*.jsx}': ['yarn lint:eslint', 'yarn lint:prettier'],
-  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['yarn lint:prettier --parser json'],
-  'package.json': ['yarn lint:prettier'],
-  '*.scss': ['yarn lint:stylelint', 'yarn lint:prettier'],
-  '*.md': ['yarn lint:prettier'],
+  '{*.js,*.jsx}': ['yarn lint:js', 'yarn format'],
+  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['yarn format --parser json'],
+  'package.json': ['yarn format'],
+  '*.scss': ['yarn lint:css', 'yarn format'],
+  '*.md': ['yarn format'],
 
   // '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged'],
 };
