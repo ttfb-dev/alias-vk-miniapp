31ea53921f92ce9a81e75a73775f3673e085ce03
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-08-10 09:31:36 +0300

    fix ios donut modal

--- src/pages/Modal/ui/Donut.jsx
+++ src/pages/Modal/ui/Donut.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
 import { useRouter } from '@happysanta/router';
 import { Icon56DonateOutline } from '@vkontakte/icons';
-import { Button, ModalCard } from '@vkontakte/vkui';
+import { Button, IOS, ModalCard, usePlatform } from '@vkontakte/vkui';
 
 import { MODAL_SETS } from '@/app/router';
 
 const Donut = (props) => {
   const router = useRouter();
 
+  const platform = usePlatform();
+
+  const isIOS = platform === IOS;
+
   const onBack = () => router.replaceModal(MODAL_SETS);
+
+  const header = !isIOS ? 'Этот набор доступен донам нашего сообщества' : 'Этот набор доступен c подпиской';
+
+  const subheader = !isIOS ? 'Поддержи наш проект, стань доном и получи полный доступ ко всем наборам!' : null;
 
   return (
     <ModalCard
       {...props}
       onClose={onBack}
       icon={<Icon56DonateOutline />}
-      header='Этот набор доступен только донам нашего сообщества'
-      subheader='Поддержи наш проект, стань доном и получи полный доступ ко всем наборам!'
+      header={header}
+      subheader={subheader}
       actions={
-        <Button size='l' mode='primary' href='https://vk.com/club204880239'>
-          Подробнее
-        </Button>
+        !isIOS ? (
+          <Button size='l' mode='primary' href='https://vk.com/club204880239'>
+            Подробнее
+          </Button>
+        ) : (
+          <Button size='l' mode='primary' onClick={onBack}>
+            Хорошо
+          </Button>
+        )
       }
     />
   );
